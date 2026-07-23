6854ca38070c7fce856edc582d646651184bb16b
Stormtroopper <raunaqjoseph@gmail.com>
2026-07-23 19:40:18 +0530

    github actions error modified -1

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,13 +3,13 @@ let mId=0;
 const nextUserId=0;
 export const addMessage=(message,author)=>({
         type:types.ADD_MESSAGE,
-            id:mId++,
+            id:mId+1,
             message,
             author
 })
 export const messageReceived=(message,author)=>({
         type:types.MESSAGE_RECEIVED,
-        id:nextUserId++,
+        id:nextUserId+1,
         message,
         author
 });
